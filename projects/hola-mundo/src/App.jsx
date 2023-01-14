99c42cef54d3df4fdf1c55493b5e87909c59f3f6
DaviniaGL <dmgl1991@gmail.com>
2023-01-14 21:19:26 +0100

    Inicio tarjeta twitter

---
+++ projects/hola-mundo/src/App.jsx
@@ -0,0 +1,13 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de midudev" src=" https://unavatar.io/midudev"></img>
+                <div>
+                    <strong>Miguel Angel</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+        </article>
+    )
+}
